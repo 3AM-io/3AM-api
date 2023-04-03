85c864b219050c03ff71ad461649b4332c2906dc
suryalays <dbvenkat857@gmail.com>
2023-04-03 22:38:55 +0530

    fix : mailclient/summarizer

--- src/webCrawl.py
+++ src/webCrawl.py
@@ -1,6 +1,6 @@
 import webbrowser
-#from googlesearch import search
 from bs4 import BeautifulSoup
+import os
 import requests
 from summerizerRes import summery
 from speak import speak
@@ -13,9 +13,10 @@ def searchSite(site: str):
     
 # Summerizer
 def getSummeryFromURL(url: str):
+    wordLimit = 20
     response = None
     try:
-        response = requests.get(str)
+        response = requests.get(url)
     except:
         speak("There's an unusual error. Try again later")
 
@@ -25,25 +26,38 @@ def getSummeryFromURL(url: str):
     # Parser
     f = open(".tempResponse.html", "r")
     parsedContent = BeautifulSoup(f, 'html.parser')
-
+    f.close()
+    os.unlink(f)
     # Get Summerized text
     resultList = []
     iter = 0
     for result in parsedContent.find('body').find_all('p'):
         iter += 1
-        resultList.append(str(result.text).replace(r"\n", "").strip())
+        resultList.append(str(result.text).replace(r"\n", " ").strip())
 
-        if iter == 1000:
+        if iter == 100:
             break
+    
+    if len(resultList) < 1:
+        for result in parsedContent.find('body').find_all('div'):
+            iter += 1
+            resultList.append(str(result.text).replace(r"\n", " ").strip())
 
-    sumerizedContent = ""
+            if iter == 100:
+                break
+    
+    summary = ''.join(resultList)
     # UnCommand this line to get the summerizer activated
     # summerizedContent = summery(''.join(resultList))
-
-    return sumerizedContent
+    # summerizedLimit = sumerizedContent.split(" ")[0:wordLimit]
+    
+    
+    return summary
 
 
 # Search Query as Keyword
 def gSearch(query: str):
     response = googleSearch(query)
-    webbrowser.open_new_tab(response["links"])
+    # webbrowser.open_new_tab(response["links"])
+    print(response["links"])
+    return getSummeryFromURL(response["links"])
