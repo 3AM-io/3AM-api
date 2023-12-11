42e8fa8faa6a8b7b13737316242671867368ac0e
suryanarayanandv <dbvenkat857@gmail.com>
2023-12-11 10:42:20 +0530

    added environment loaders

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
+import os
 from app import responseGen
 from speechrecognition import takeCommand
+from dotenv import load_dotenv
+from setup import setupEnvironment
 
 if __name__ == '__main__':
+    # Local environment setup
+    load_dotenv()
+    senderMail = os.environ.get('SENDER_MAIL')
+    if senderMail == "example123@gmail.com":
+        setupEnvironment()
+
     query = takeCommand()
     responseGen(query)
